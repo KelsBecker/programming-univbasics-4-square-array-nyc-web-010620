96cde46447e892ce84b5da0d4684919a2efa7f82
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-11 01:54:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
- new numbers = []
  counter = 0
   while array[counter] do
-    array.shift
-    puts array[counter]
+    new_num = array.shift **2
+    array.push new_num
     counter += 1
   end
+  array
 end
